8f64ca20ca23487790bc2f74d4a4fbf4bc5fab43
XinYangXY <114933362+XinYangXY@users.noreply.github.com>
2022-10-25 09:46:47 +0800

    Start Screen

---
+++ core/src/com/example/game/StartScreen.java
@@ -0,0 +1,62 @@
+package com.example.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+public class StartScreen implements Screen {
+
+    final MainGame game;
+    OrthographicCamera camera;
+
+    public StartScreen(final MainGame maingame) {
+
+        game = maingame;
+
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, 800, 480);
+    }
+
+    @Override
+    public void render(float delta) {
+        ScreenUtils.clear(255, 255, 255, 1);
+
+        camera.update();
+        game.batch.setProjectionMatrix(camera.combined);
+
+        game.batch.begin();
+        game.font.draw(game.batch, "Welcome to Flappy Bird", 160, 320);
+        game.font.draw(game.batch, "Tap anywhere to begin", 180, 230);
+        game.batch.end();
+
+        if (Gdx.input.isTouched()) {
+            game.setScreen(new MainMenuScreen(game));
+            dispose();
+        }
+    }
+
+    @Override
+    public void resize(int width, int height) {
+    }
+
+    @Override
+    public void show() {
+    }
+
+    @Override
+    public void hide() {
+    }
+
+    @Override
+    public void pause() {
+    }
+
+    @Override
+    public void resume() {
+    }
+
+    @Override
+    public void dispose() {
+    }
+}
