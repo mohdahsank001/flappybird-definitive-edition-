1b3dda7718ecff2e52a350e2eab0e96b0cc4fc9d
Vaibhavi Kulkarni <vaibhavik@student.unimelb.edu.au>
2022-11-01 01:33:18 +1100

    Initial weather Code

--- README.md
+++ README.md
@@ -1,32 +1,3 @@
-# flappybird-definitive-edition-
-definitive edition of the classic flappy bird game developed by team065 at unimelb for COMP90018 Assignment Component.
-
-
-
- <img width="176" alt="image" src="https://user-images.githubusercontent.com/67852641/194203134-1e0ff252-7c47-4d62-894e-63d01c65d30a.png">
-<br>
-
-<img width="835" alt="image" src="https://user-images.githubusercontent.com/67852641/194204911-642b6f13-3dff-4512-818b-0344280a8149.png">
-<br>
-
-<img width="1419" alt="image" src="https://user-images.githubusercontent.com/67852641/197109185-bfea9489-92a5-43bf-97d5-cf4b3b9d7e90.png">
-<br>
-
-<img width="1466" alt="image" src="https://user-images.githubusercontent.com/67852641/197109242-b02370e4-32bd-45ce-8073-27992d4d0347.png">
-
-<br>
-<img width="1466" alt="image" src="https://user-images.githubusercontent.com/67852641/197109327-013df3c2-dd98-41c4-9ef3-90d4c8680d49.png">
-
-<br>
-<img width="1466" alt="image" src="https://user-images.githubusercontent.com/67852641/197109384-c53e7444-4dcf-4189-b333-92d7328244ac.png">
-
-<br>
-<img width="1466" alt="image" src="https://user-images.githubusercontent.com/67852641/197109439-0fb3507a-6bd3-4265-963f-090e7c24bd70.png">
-
-<br>
-Introducing floating tubes- In the new game, tubes are never attached to a surface but rather float in air. 
-We wanted users to score through the game, so we have introduced a new feature. In certain pipe configuration, you will be able to move below or above the tube. 
-<img width="1466" alt="image" src="https://user-images.githubusercontent.com/67852641/197379660-f8ae49a3-1828-4b51-a81c-648ceb4c096f.png">
-
-
-
+# Weather-App
+In this project, we will be building a weather application. This application will show the temperature of a location. To fetch weather information we will need an API. An API(Application Programming Interface) is a function that allows applications to interact and share data using various components and microservices. For this project, we will be using WeatherBit API for fetching weather data. WeatherBit API provides a fast and elegant way to fetch weather data.
+We will build an app that will find the device’s location coordinates(longitude and latitude). Then we will send this data to the API via an API key(which we will see later). The API will send us a JSON from which we will extract the required data that is the temperature and city of the location.
---
+++ app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -0,0 +1,93 @@
+package com.example.weatherapp;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+    String CITY = "Melbourne,AU";
+    String API = "eb0583e158772e681a76f22d99d07ca0";
+
+    TextView addressTxt, updated_atTxt, statusTxt, tempTxt;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        addressTxt = findViewById(R.id.address);
+        updated_atTxt = findViewById(R.id.updated_at);
+        statusTxt = findViewById(R.id.status);
+        tempTxt = findViewById(R.id.temp);
+
+        new weatherTask().execute();
+
+    }
+
+
+    class weatherTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            /* Showing the ProgressBar, Making the main design GONE */
+            findViewById(R.id.loader).setVisibility(View.VISIBLE);
+            // findViewById(R.id.mainContainer).setVisibility(View.GONE);
+            findViewById(R.id.errorText).setVisibility(View.GONE);
+        }
+
+        protected String doInBackground(String... args) {
+            String response = HttpRequest.excuteGet("http://api.openweathermap.org/data/2.5/weather?q=" +CITY+ "&units=metric&appid=eb0583e158772e681a76f22d99d07ca0");
+            return response;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+
+
+            try {
+                JSONObject jsonObj = new JSONObject(result);
+                JSONObject main = jsonObj.getJSONObject("main");
+                JSONObject sys = jsonObj.getJSONObject("sys");
+                //JSONObject wind = jsonObj.getJSONObject("wind");
+                JSONObject weather = jsonObj.getJSONArray("weather").getJSONObject(0);
+
+                Long updatedAt = jsonObj.getLong("dt");
+                String updatedAtText = "Updated at: " + new SimpleDateFormat("dd/MM/yyyy hh:mm a", Locale.ENGLISH).format(new Date(updatedAt * 1000));
+                String temp = main.getString("temp") + "°C";
+                String weatherDescription = weather.getString("description");
+
+                String address = jsonObj.getString("name") + ", " + sys.getString("country");
+
+
+                /* Populating extracted data into our views */
+                addressTxt.setText(address);
+                updated_atTxt.setText(updatedAtText);
+                statusTxt.setText(weatherDescription.toUpperCase());
+                tempTxt.setText(temp);
+
+
+                /* Views populated, Hiding the loader, Showing the main design */
+                findViewById(R.id.loader).setVisibility(View.GONE);
+               // findViewById(R.id.mainContainer).setVisibility(View.VISIBLE);
+
+
+            } catch (JSONException e) {
+                findViewById(R.id.loader).setVisibility(View.GONE);
+                findViewById(R.id.errorText).setVisibility(View.VISIBLE);
+            }
+
+        }
+    }
+}
