70dbe53a5e9814d97a097a2f249679f72b453886
wzx6622 <82555681+wzx6622@users.noreply.github.com>
2022-10-31 12:02:37 +1100

    Add files via upload

---
+++ android/src/com/example/game/AndroidInterfaceBluetoothClass.java
@@ -0,0 +1,103 @@
+package com.example.game;
+
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.SystemClock;
+import android.view.InputDevice;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class AndroidInterfaceBluetoothClass implements BluetoothInterface{
+
+    private Activity context;
+
+    public AndroidInterfaceBluetoothClass(Activity context) {
+        this.context = context;
+    }
+
+    @Override
+    public void bluetoothUnavailableHandler() {
+        context.runOnUiThread(new Runnable() {
+            @TargetApi(Build.VERSION_CODES.M)
+            @Override
+            public void run() {
+                BluetoothAdapter BA = BluetoothAdapter.getDefaultAdapter();
+                int intVal = 2;
+                if (BA == null) {
+                    Toast.makeText(context.getApplicationContext(), "Bluetooth unavailable in this device", Toast.LENGTH_SHORT).show();
+                } else {
+                    if (BA.isEnabled()) {
+                        if (getGameControllerIds().isEmpty()) {
+                            Toast.makeText(context.getApplicationContext(), "No controller found", Toast.LENGTH_SHORT).show();
+                        }
+                        else {
+                            return;
+                        }
+                    }
+                    else {
+                        if (context.checkSelfPermission(Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+                            context.requestPermissions(new String[] {Manifest.permission.BLUETOOTH_CONNECT},1);
+                            return;
+                        }
+                        else {
+                            Intent i = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                            context.startActivityForResult(i, intVal);
+                        }
+                    }
+                }
+            }
+        });
+    }
+
+    @Override
+    public boolean isControllerAvailable() {
+        if (!getGameControllerIds().isEmpty()) {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void bluetoothAvailableMsg() {
+        context.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(context.getApplicationContext(), "Controller connected", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public void bluetoothUnavailableMsg() {
+        context.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(context.getApplicationContext(), "Controller disconnected", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    public ArrayList<Integer> getGameControllerIds() {
+        ArrayList<Integer> gameControllerDeviceIds = new ArrayList<Integer>();
+        int[] deviceIds = InputDevice.getDeviceIds();
+        for (int deviceId : deviceIds) {
+            InputDevice dev = InputDevice.getDevice(deviceId);
+            int sources = dev.getSources();
+            if (((sources & InputDevice.SOURCE_GAMEPAD) == InputDevice.SOURCE_GAMEPAD)
+                    || ((sources & InputDevice.SOURCE_JOYSTICK)
+                    == InputDevice.SOURCE_JOYSTICK)) {
+                if (!gameControllerDeviceIds.contains(deviceId)) {
+                    gameControllerDeviceIds.add(deviceId);
+                }
+            }
+        }
+        return gameControllerDeviceIds;
+    }
+
+}
